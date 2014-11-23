b498d45243506d578c6bb0973e7176667ca57235
Enrique Bonilla <enriqueBonilla93@outlook.com>
2014-11-22 22:58:03 -0600

    Fixed some stuff

--- Spaceship_Armada_SANDBOX/Spaceship_Armada_SANDBOX/GameScene.m
+++ Spaceship_Armada_SANDBOX/Spaceship_Armada_SANDBOX/GameScene.m
@@ -53,7 +53,7 @@ CMMotionManager *motionManager;
     _scoreNode = [SKLabelNode labelNodeWithFontNamed:@"Arial"];
     _scoreNode.text=[NSString stringWithFormat:@"Score: %ld",(long)_score];
     _scoreNode.fontSize = 30;
-    _scoreNode.position = CGPointMake(self.size.width-140, self.size.height-100);
+    _scoreNode.position = CGPointMake(self.size.width-140, self.size.height-30);
     [self addChild:_scoreNode];
     
     self.physicsWorld.gravity = CGVectorMake(0, 0);
